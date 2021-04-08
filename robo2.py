4a1c4917b8621f9c2a94366eb711bf38aa350ec7
Unknown <y.creak@hotmail.com>
2021-04-08 12:32:06 +0200

    Color adjustments

--- robo2.py
+++ robo2.py
@@ -31,14 +31,18 @@ while(1):
   
     # Set range for green color and 
     # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
+    green_lower = np.array([25, 100, 72], np.uint8)
+    green_upper = np.array([90, 255, 200], np.uint8)
+    # green_lower = np.array([25, 52, 72], np.uint8)
+    # green_upper = np.array([102, 255, 255], np.uint8)    
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
   
     # Set range for blue color and
     # define mask
     blue_lower = np.array([94, 80, 2], np.uint8)
     blue_upper = np.array([120, 255, 255], np.uint8)
+    # blue_lower = np.array([94, 80, 2], np.uint8)
+    # blue_upper = np.array([120, 255, 255], np.uint8)
     blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
       
     # Morphological Transform, Dilation
@@ -74,43 +78,45 @@ while(1):
             imageFrame = cv2.rectangle(imageFrame, (x, y), 
                                        (x + w, y + h), 
                                        (0, 0, 255), 2)
-              
+            print('Coordinates: {0}-{1}'.format(x,y))              
             cv2.putText(imageFrame, "Red Colour", (x, y),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.0,
                         (0, 0, 255))    
   
     # # Creating contour to track green color
-    # contours, hierarchy = cv2.findContours(green_mask,
-    #                                        cv2.RETR_TREE,
-    #                                        cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(green_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
       
-    # for pic, contour in enumerate(contours):
-    #     area = cv2.contourArea(contour)
-    #     if(area > 300):
-    #         x, y, w, h = cv2.boundingRect(contour)
-    #         imageFrame = cv2.rectangle(imageFrame, (x, y), 
-    #                                    (x + w, y + h),
-    #                                    (0, 255, 0), 2)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                       (x + w, y + h),
+                                       (0, 255, 0), 2)
               
-    #         cv2.putText(imageFrame, "Green Colour", (x, y),
-    #                     cv2.FONT_HERSHEY_SIMPLEX, 
-    #                     1.0, (0, 255, 0))
+
+
+            cv2.putText(imageFrame, "Green Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        1.0, (0, 255, 0))
   
-    # # Creating contour to track blue color
-    # contours, hierarchy = cv2.findContours(blue_mask,
-    #                                        cv2.RETR_TREE,
-    #                                        cv2.CHAIN_APPROX_SIMPLE)
-    # for pic, contour in enumerate(contours):
-    #     area = cv2.contourArea(contour)
-    #     if(area > 300):
-    #         x, y, w, h = cv2.boundingRect(contour)
-    #         imageFrame = cv2.rectangle(imageFrame, (x, y),
-    #                                    (x + w, y + h),
-    #                                    (255, 0, 0), 2)
+    # Creating contour to track blue color
+    contours, hierarchy = cv2.findContours(blue_mask,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area > 300):
+            x, y, w, h = cv2.boundingRect(contour)
+            imageFrame = cv2.rectangle(imageFrame, (x, y),
+                                       (x + w, y + h),
+                                       (255, 0, 0), 2)
               
-    #         cv2.putText(imageFrame, "Blue Colour", (x, y),
-    #                     cv2.FONT_HERSHEY_SIMPLEX,
-    #                     1.0, (255, 0, 0))
+            cv2.putText(imageFrame, "Blue Colour", (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1.0, (255, 0, 0))
               
     # Program Termination
     cv2.imshow("Multiple Color Detection in Real-Time", imageFrame)
